3b08dfb31dac5fabdf7f396247dc92abadf77fc4
Hua-Quan Chen <87644334@qq.com>
2020-12-01 23:43:11 +0800

    batch upload dec 1

---
+++ comsume-restservice/src/test/java/com/bigdata/comsumerestservice/ComsumeRestserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigdata.comsumerestservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComsumeRestserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ upload-file/src/main/java/com/bigdata/uploadfile/service/StorageProperties.java
@@ -0,0 +1,24 @@
+package com.bigdata.uploadfile.service;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties("storage")
+public class StorageProperties {
+
+	/**
+	 * Folder location for storing files
+	 */
+	private String location = "upload-dir";
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+}
