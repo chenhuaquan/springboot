0037c0aa916a383a7c27703e2797ff7d3a2e9894
jusda-sh-d-glendonchen <glendon.chen@mail.jusdascm.com>
2020-12-04 23:08:35 +0800

    redis sample

---
+++ simple-redis/src/test/java/com/bigdata/simpleredis/SimpleRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigdata.simpleredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
